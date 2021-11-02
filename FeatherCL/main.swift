ff56c20b4372735b0206abf8a4793fdecec07ac1
Emlyn Murphy <me@emlyn.net>
2021-11-02 06:35:31 -0400

    Get authorize URL

--- FeatherCL/main.swift
+++ FeatherCL/main.swift
@@ -6,6 +6,41 @@
 //
 
 import Foundation
+import FeatherKit
+import Combine
 
-print("Hello, World!")
+var cancellable = [AnyCancellable]()
 
+Authentication.default.requestTokenPublisher().sink { completion in
+    switch completion {
+    case .finished:
+        print("finished")
+    case .failure(let error):
+        print("error: \(error)")
+    }
+
+    cancellable.removeAll()
+} receiveValue: { token in
+    print("got token: \(token)")
+
+    guard let authorizeURL = URL.Provider.default.authorize(oauthToken: token.oauthToken) else {
+        print("Couldn't get URL")
+        return
+    }
+
+    print("authorize URL: \(authorizeURL)")
+}.store(in: &cancellable)
+
+let isAuth = Authentication.default.isAuthenticated
+print("isAuth: \(isAuth)")
+
+while(cancellable.count > 0) { }
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
